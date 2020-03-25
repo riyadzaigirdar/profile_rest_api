09431467560754904cd5f70244d7d8dd076634ee
riyadzaigirdar <riyadzaigir280@gmail.com>
2020-03-25 10:46:37 +0600

    token modheader

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path,include
 from .views import HelloApiView
 from .views import UserViewSet
+from .views import UserLoginApi
 from rest_framework.routers import DefaultRouter
 
 
@@ -12,6 +13,7 @@ router.register('hello_api_viewset', UserViewSet, basename='viewset')
 
 urlpatterns = [
     path('hello_api_view/', HelloApiView.as_view()),
+    path('login/', UserLoginApi.as_view()),
     path('',include(router.urls))
 ]
 
--- profile_app/views.py
+++ profile_app/views.py
@@ -3,7 +3,10 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from profile_app.permissions import UserProfilePermission 
 from .serializers import HelloSerializer,UserProfileSerializer
 from .models import UserProfile
@@ -49,7 +52,11 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     authentication_classes = [TokenAuthentication]   
     permission_classes = [UserProfilePermission]
+    filter_backends = [SearchFilter]
+    search_fields = ['id','email','name']
     
-        
+class UserLoginApi(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
    
 
